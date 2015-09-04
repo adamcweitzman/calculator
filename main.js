2504281db2c98efae95b0de81824ba5034f6b2bd
Adam Weitzman <adamcweitzman@gmail.com>
2015-09-04 11:17:36 -0500

    almostdone

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-<input id="output"
+$(function () {
+
+
+var number1;
+var number2;
+var operator;
+
+
+$('#calculate').on('click', function() {
+	number1 = parseInt($('#number1').val())
+	number2 = parseInt($('#number2').val())
+	
+	if ()
+
+
+
+	});
+});
