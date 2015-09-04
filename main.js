639aaa2dd2055a67b118692bb5b59a661e10ba83
Adam Weitzman <adamcweitzman@gmail.com>
2015-09-04 11:39:25 -0500

    works with +

--- main.js
+++ main.js
@@ -10,7 +10,17 @@ $('#calculate').on('click', function() {
 	number1 = parseInt($('#number1').val())
 	number2 = parseInt($('#number2').val())
 	
-	if ()
+	if ("+") {
+		$('#display').html(number1 + number2)
+	
+	} else if ("-") {
+		$('#display').html(number1 - number2)
+	}
+
+	// if ("/") {
+	// 	$('#display').html(number1 % number2)
+	// }
+
 
 
 
